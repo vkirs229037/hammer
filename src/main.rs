0e009d424689fcf60496f8dae76caa10b2211d03
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-10-31 00:24:54 +0300

    Реализация конструктора и запуска программы

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod errors;
+use errors::*;
+
 type Value = f64;
 
 enum Instruction {
@@ -20,7 +23,7 @@ enum Instruction {
 }
 
 impl TryFrom<u8> for Instruction {
-    type Error = &'static str;
+    type Error = OpcodeError;
 
     fn try_from(byte: u8) -> Result<Self, Self::Error> {
         match byte {
@@ -40,16 +43,107 @@ impl TryFrom<u8> for Instruction {
             0x0d => Ok(Instruction::RET),
             0x0e => Ok(Instruction::DBG),
             0x0f => Ok(Instruction::HLT),
-            _ => Err("Not yet handled opcode"),
+            _ => Err(OpcodeError),
         }
     }
 }
 
 struct VM {
     stack: Vec<Value>,
-    program: Vec<Instruction>,
+    program: Vec<u8>,
     consts: Vec<Value>,
-    pc: u32,
+    pc: usize,
+    running: bool,
+}
+
+impl VM {
+    fn new() -> Self {
+        VM {
+            stack: vec![],
+            program: vec![],
+            consts: vec![],
+            pc: 0,
+            running: false,
+        }
+    }
+
+    fn add_const(&mut self, value: Value) {
+        self.consts.push(value);
+    }
+
+    fn run(&mut self) -> Result<(), InterpretationError> {
+        self.running = true;
+        while self.running {
+            self.run_one_instr()?;
+        }
+        Ok(())
+    }
+
+    fn run_one_instr(&mut self) -> Result<(), InterpretationError> {
+        let inst: Instruction = self.program.get(self.pc)
+                                            .ok_or_else(|| InterpretationError::UnexpectedEndError(UnexpectedEndError))?
+                                            .clone()
+                                            .try_into()?;
+        match inst {
+            Instruction::NOP => { },
+            Instruction::PUSH => {
+                let b1: u8 = *self.program.get(self.pc+1)
+                                          .ok_or_else(|| InterpretationError::UnexpectedEndError(UnexpectedEndError))?;
+                let b2: u8 = *self.program.get(self.pc+2)
+                                          .ok_or_else(|| InterpretationError::UnexpectedEndError(UnexpectedEndError))?;
+                let index: u16 = u16::from_ne_bytes([b1, b2]);
+                let val: Value = *self.consts.get(index as usize)
+                                             .ok_or_else(|| InterpretationError::BadConstsIndexError(BadConstsIndexError))?;
+                self.stack.push(val);
+                
+            },
+            Instruction::ADD => {
+                let a = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                let b = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                self.stack.push(a + b);
+            },
+            Instruction::SUB => {
+                let a = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                let b = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                self.stack.push(a - b);
+            },
+            Instruction::MUL => {
+                let a = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                let b = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                self.stack.push(a * b);
+            },
+            Instruction::DIV => {
+                let a = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                let b = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                self.stack.push(a / b);
+            },
+            Instruction::JMP => todo!("Не реализованы"),
+            Instruction::JE => todo!("Не реализованы"),
+            Instruction::JNE => todo!("Не реализованы"),
+            Instruction::JG => todo!("Не реализованы"),
+            Instruction::JL => todo!("Не реализованы"),
+            Instruction::JGE => todo!("Не реализованы"),
+            Instruction::JLE => todo!("Не реализованы"),
+            Instruction::RET => todo!("Не реализованы"),
+            Instruction::DBG => {
+                let a = self.stack.pop()
+                                  .ok_or_else(|| InterpretationError::EmptyStackError(EmptyStackError))?;
+                println!("{a:#}");
+            },
+            Instruction::HLT => {
+                self.running = false;
+            },
+        }
+        Ok(())
+    }
 }
 
 fn main() {
