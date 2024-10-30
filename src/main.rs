80536c8d4170253b365040cfad33f5c53dff380f
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-10-30 22:16:18 +0300

    enum для инструкций, struct для виртуальной машины

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+type Value = f64;
+
+enum Instruction {
+    PUSH(Value),
+    ADD,
+    SUB,
+    MUL,
+    DIV,
+}
+
+
+struct VM {
+    stack: Vec<Value>,
+    program: Vec<Instruction>,
+}
+
 fn main() {
     println!("Hello, world!");
 }
