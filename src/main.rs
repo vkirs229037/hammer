dc1c1320d6efa0e686e8a0f8fa47fb36205f2962
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-10-31 23:52:09 +0300

    Тестовая программа с условным переходом

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,21 @@ use vm::*;
 
 fn main() {
     let mut vm = VM::new();
-    vm.add_const(1f64);
-    vm.add_const(3f64);
+    vm.add_const(10f64);
+    vm.add_const(20f64);
     vm.add_const(0f64);
-    let program: Vec<u8> = vec![0x01,       // PUSH
-                                0x00, 0x00, // 0x0000 -> 1
-                                0x01,       // PUSH
-                                0x01, 0x00, // 0x0001 -> 3
-                                0x02,       // ADD
-                                0x01,       // PUSH
-                                0x02, 0x00, // 0x0002 -> 0
-                                0x05,       // DIV -> Ошибка о делении на 0
-                                0x0e,       // DBG
-                                0x0f];      // HLT
+    vm.add_const(1f64);
+    let program: Vec<u8> = vec![
+        0x01, 0x00, 0x00, // PUSH 0x0000 -> 10
+        0x01, 0x01, 0x00, // PUSH 0x0001 -> 10 20
+        0x09, 0x08, 0x00, // JG 0x0008 
+        0x01, 0x02, 0x00, // PUSH 0x0002 -> 10 20 0
+        0x0e,             // DBG -> 0
+        0x0f,             // HLT
+        0x01, 0x03, 0x00, // PUSH 0x0003 -> 10 20 1
+        0x0e,             // DBG -> 1
+        0x0f,             // HLT
+    ];
     vm.load_program(program);
     
     let run_result = vm.run();
