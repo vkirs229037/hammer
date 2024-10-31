d76a86b5db58c827ed897ef85b9a5d0384e6d315
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-01 00:26:23 +0300

    2 тестовые программы

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,35 @@ use vm::*;
 
 fn main() {
     let mut vm = VM::new();
-    vm.add_const(10f64);
     vm.add_const(20f64);
-    vm.add_const(0f64);
-    vm.add_const(1f64);
-    let program: Vec<u8> = vec![
-        0x01, 0x00, 0x00, // PUSH 0x0000 -> 10
-        0x01, 0x01, 0x00, // PUSH 0x0001 -> 10 20
-        0x09, 0x08, 0x00, // JG 0x0008 
-        0x01, 0x02, 0x00, // PUSH 0x0002 -> 10 20 0
-        0x0e,             // DBG -> 0
+    vm.add_const(10f64);
+    // if (20 > 10) { 20 + 10 } else { 20 - 10 }
+    let _program1: Vec<u8> = vec![
+        0x01, 0x00, 0x00, // PUSH 0 -> 20
+        0x01, 0x01, 0x00, // PUSH 1 -> 20 10
+        0x09, 0x0c, 0x00, // JG 12 
+        0x01, 0x00, 0x00, // PUSH 0 -> 20
+        0x01, 0x01, 0x00, // PUSH 1 -> 20 10
+        0x03,             // SUB -> 10
+        0x0e,             // DBG -> 10
         0x0f,             // HLT
-        0x01, 0x03, 0x00, // PUSH 0x0003 -> 10 20 1
-        0x0e,             // DBG -> 1
+        0x01, 0x00, 0x00, // PUSH 0 -> 20
+        0x01, 0x01, 0x00, // PUSH 1 -> 20 10
+        0x02,             // ADD -> 30
+        0x0e,             // DBG -> 30
         0x0f,             // HLT
     ];
-    vm.load_program(program);
+
+    let program2: Vec<u8> = vec![
+        0x01, 0x00, 0x00, // PUSH 0 -> 20
+        0x06, 0x04, 0x00, // JMP 3
+        0x0f,             // HLT
+        0x01, 0x01, 0x00, // PUSH 1 -> 20 10
+        0x02,             // ADD -> 30
+        0x0e,             // DBG -> 30
+        0x0f,             // HLT
+    ];
+    vm.load_program(program2);
     
     let run_result = vm.run();
     match run_result {
