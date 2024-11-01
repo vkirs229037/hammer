183fa4839848063c792d9be39457f6024bbc0440
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-01 19:15:49 +0300

    Удаление ошибок о скобках и неожиданном eof из LexError

--- src/parser/errors.rs
+++ src/parser/errors.rs
@@ -4,8 +4,6 @@ use crate::parser::tokens::Loc;
 pub enum LexError {
     MalformedNumLit(Loc),
     UnexpectedToken(Loc),
-    UnmatchedParens(Loc),
-    UnexpectedEOF(Loc),
     UnknownLexem(Loc),
 }
 
@@ -14,8 +12,6 @@ impl fmt::Display for LexError {
         match self {
             Self::MalformedNumLit(loc) => write!(f, "[{loc}] неправильный float литерал"),
             Self::UnexpectedToken(loc) => write!(f, "[{loc}] неожиданное появление"),
-            Self::UnmatchedParens(loc) => write!(f, "[{loc}] скобки были неверно расставлены"),
-            Self::UnexpectedEOF(loc) => write!(f, "[{loc}] неожиданный конец файла"),
             Self::UnknownLexem(loc) => write!(f, "[{loc}] неизвестная лексема"),
         }
     }
