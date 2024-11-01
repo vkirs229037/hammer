b4db8ff2803f1e44723ae21a5eb037aa97fd38bb
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-01 19:22:01 +0300

    Удаление ошибки о неожиданном появлении из LexError

--- src/parser/errors.rs
+++ src/parser/errors.rs
@@ -3,7 +3,6 @@ use crate::parser::tokens::Loc;
 
 pub enum LexError {
     MalformedNumLit(Loc),
-    UnexpectedToken(Loc),
     UnknownLexem(Loc),
 }
 
@@ -11,7 +10,6 @@ impl fmt::Display for LexError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::MalformedNumLit(loc) => write!(f, "[{loc}] неправильный float литерал"),
-            Self::UnexpectedToken(loc) => write!(f, "[{loc}] неожиданное появление"),
             Self::UnknownLexem(loc) => write!(f, "[{loc}] неизвестная лексема"),
         }
     }
