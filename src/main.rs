8238e0bd915c247f859888661e34aca247aba538
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-01 13:39:53 +0300

    Реализация команд сравнения и команд JF, JBACK

--- src/main.rs
+++ src/main.rs
@@ -9,32 +9,23 @@ fn main() {
     vm.add_const(20f64);
     vm.add_const(10f64);
     // if (20 > 10) { 20 + 10 } else { 20 - 10 }
-    let _program1: Vec<u8> = vec![
+    let program: Vec<u8> = vec![
         0x01, 0x00, 0x00, // PUSH 0 -> 20
         0x01, 0x01, 0x00, // PUSH 1 -> 20 10
-        0x09, 0x0c, 0x00, // JG 12 
-        0x01, 0x00, 0x00, // PUSH 0 -> 20
-        0x01, 0x01, 0x00, // PUSH 1 -> 20 10
-        0x03,             // SUB -> 10
-        0xfe,             // DBG -> 10
+        0x09,             // GR     -> 20 > 10 -> 1
+        0x0e, 0x0c, 0x00, // JF 12
+        0x01, 0x00, 0x00, // PUSH 0
+        0x01, 0x01, 0x00, // PUSH 1
+        0x02,             // ADD
+        0xfe,             // DBG
         0xff,             // HLT
-        0x01, 0x00, 0x00, // PUSH 0 -> 20
-        0x01, 0x01, 0x00, // PUSH 1 -> 20 10
-        0x02,             // ADD -> 30
-        0xfe,             // DBG -> 30
-        0xff,             // HLT
-    ];
-
-    let program2: Vec<u8> = vec![
-        0x01, 0x00, 0x00, // PUSH 0 -> 20
-        0x06, 0x04, 0x00, // JMP 3
-        0xff,             // HLT
-        0x01, 0x01, 0x00, // PUSH 1 -> 20 10
-        0x02,             // ADD -> 30
-        0xfe,             // DBG -> 30
+        0x01, 0x00, 0x00, // PUSH 0
+        0x01, 0x01, 0x00, // PUSH 1
+        0x03,             // SUB
+        0xfe,             // DBG
         0xff,             // HLT
     ];
-    vm.load_program(program2);
+    vm.load_program(program);
     
     let run_result = vm.run();
     match run_result {
