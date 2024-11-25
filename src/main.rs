3e27213ffa85bfe81a2004ba0731a9ff6f916867
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-25 17:49:15 +0300

    Тестовая программа

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ mod compile;
 use parser::lexer::Lexer;
 use parser::ast::{AstBuilder, AstNode};
 use compile::compiler::Compiler;
+use vm::vm::VM;
+
+use std::fs;
+use std::io::Read;
 
 fn main() {
     /*
@@ -37,7 +41,8 @@ fn main() {
         Err(e) => println!("Ошибка: {e}"),
     }
     */
-    let program = String::from("-4 / 3 + 6 * (7 - 1);");
+    let program = String::from("(25 / 5 * 5 - (6 - 5)) + 5 + (25 * 5 - 5 * (5 - 3));");
+
     let mut lexer = Lexer::new("module".to_owned(), program);
     match lexer.lex() {
         Ok(()) => {
@@ -49,6 +54,7 @@ fn main() {
         },
         Err(e) => println!("Ошибка: {e}")
     }
+
     let mut ast_builder = AstBuilder::new(lexer.tokens().to_vec());
     match ast_builder.parse() {
         Ok(()) => { 
@@ -58,6 +64,7 @@ fn main() {
         },
         Err(e) => println!("Ошибка: {e}"),
     };
+
     let tree = ast_builder.tree();
     let mut compiler = match Compiler::new(&tree, String::from("out/out")) {
         Ok(c) => c,
@@ -67,6 +74,22 @@ fn main() {
         }
     };
     match compiler.compile() {
+        Ok(()) => println!("Все хорошо:)"),
+        Err(e) => println!("Ошибка: {e}"),
+    }
+
+    let mut vm = VM::new();
+    let mut file = match fs::OpenOptions::new().read(true).open("out/out") {
+        Ok(f) => f,
+        Err(e) => panic!("{e}"),
+    };
+    let mut program: Vec<u8> = vec![];
+    file.read_to_end(&mut program);
+    vm.load_program(program);
+    for c in compiler.consts() {
+        vm.add_const(*c);
+    }
+    match vm.run() {
         Ok(()) => println!("Все хорошо:)"),
         Err(e) => println!("Ошибка: {e}"),
     }
