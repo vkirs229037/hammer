e6d321e44389fb5fedf590e0cd3036557cacd3d3
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-25 17:58:41 +0300

    Убрал старую тестовую программу

--- src/main.rs
+++ src/main.rs
@@ -12,35 +12,6 @@ use std::fs;
 use std::io::Read;
 
 fn main() {
-    /*
-    let mut vm = VM::new();
-    vm.add_const(20f64);
-    vm.add_const(10f64);
-    // if (20 > 10) { 20 + 10 } else { 20 - 10 }
-    let program: Vec<u8> = vec![
-        0x01, 0x00, 0x00, // PUSH 0 -> 20
-        0x01, 0x01, 0x00, // PUSH 1 -> 20 10
-        0x09,             // GR     -> 20 > 10 -> 1
-        0x0e, 0x0c, 0x00, // JF 12
-        0x01, 0x00, 0x00, // PUSH 0
-        0x01, 0x01, 0x00, // PUSH 1
-        0x02,             // ADD
-        0xfe,             // DBG
-        0xff,             // HLT
-        0x01, 0x00, 0x00, // PUSH 0
-        0x01, 0x01, 0x00, // PUSH 1
-        0x03,             // SUB
-        0xfe,             // DBG
-        0xff,             // HLT
-    ];
-    vm.load_program(program);
-    
-    let run_result = vm.run();
-    match run_result {
-        Ok(()) => println!("Все хорошо:)"),
-        Err(e) => println!("Ошибка: {e}"),
-    }
-    */
     let program = String::from("(25 / 5 * 5 - (6 - 5)) + 5 + (25 * 5 - 5 * (5 - 3));");
 
     let mut lexer = Lexer::new("module".to_owned(), program);
