eefb34fa8fd0b31c96da11e096e53f89073247e8
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-25 16:19:59 +0300

    Ошибка "ожидалась операция"

--- src/compile/errors.rs
+++ src/compile/errors.rs
@@ -4,12 +4,15 @@ use std::io;
 
 pub enum CompileError {
     FileError(String, io::Error),
+    ExpectedOp(Loc)
 }
 
 impl fmt::Display for CompileError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Self::FileError(filename, error) => write!(f, "[{filename}] ошибка: {error}")
+            Self::FileError(filename, error) => write!(f, "[{filename}] ошибка: {error}"),
+            Self::ExpectedOp(loc) => write!(f, "[{loc}] ошибка: ожидался знак операции")
         }
     }
 }
+
