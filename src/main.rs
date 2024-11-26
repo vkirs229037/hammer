28e05e5dcdb04ad35bf8b6d0e7580a390920e8d9
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-26 20:06:05 +0300

    Функция main()

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod compile;
 mod cli;
 mod error;
 
+use cli::Cli;
+use error::HammerError;
 use parser::lexer::Lexer;
 use parser::ast::{AstBuilder, AstNode};
 use compile::compiler::Compiler;
@@ -13,7 +15,20 @@ use vm::vm::VM;
 use std::fs;
 use std::io::Read;
 use std::env;
+use std::process::ExitCode;
 
-fn main() {
-    println!("a");
+fn main() -> ExitCode {
+    let mut args = env::args();
+    let cli = match Cli::new(&mut args) {
+        Ok(c) => c,
+        Err(e) => {
+            println!("{e}");
+            Cli::usage();
+            return ExitCode::FAILURE
+        }
+    };
+    match cli.run() {
+        Ok(()) => ExitCode::SUCCESS,
+        Err(e) => ExitCode::FAILURE,
+    }
 }
