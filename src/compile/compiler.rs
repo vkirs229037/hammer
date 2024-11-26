0775cfbb8bdf067b34af8f12314e2d59b3422833
vkirs229037 <vova.kirsanov.03@mail.ru>
2024-11-26 11:53:23 +0300

    Изменение структуры компилятора: выходной файл больше не является ее частью

--- src/compile/compiler.rs
+++ src/compile/compiler.rs
@@ -9,22 +9,15 @@ use std::io::{self, Write};
 pub struct Compiler<'c> {
     tree: &'c AstNode,
     current_subtree: &'c AstNode,
-    out_file: fs::File,
     file_name: String,
     const_table: Vec<Value>
 }
 
 impl<'c> Compiler<'c> {
     pub fn new(tree: &'c AstNode, out_file_path: String) -> Result<Self, CompileError>  {
-        let path = path::Path::new(&out_file_path);
-        let file = fs::OpenOptions::new().write(true)
-                                         .create(true)
-                                         .open(path)
-                                         .map_err(|e| CompileError::FileError(out_file_path.clone(), e))?;
         let mut compiler = Self { 
             tree: &AstNode::None,
             current_subtree: &AstNode::None,
-            out_file: file,
             file_name: out_file_path,
             const_table: vec![]
         };
@@ -34,37 +27,43 @@ impl<'c> Compiler<'c> {
     }
 
     pub fn compile(&mut self) -> Result<(), CompileError> {
-        self.compile_expr()?;
-        self.write_out(&[0xfe])?;
-        self.write_out(&[0xff])
+        let path = path::Path::new(&self.file_name);
+        let mut file = fs::OpenOptions::new().write(true)
+                                             .create(true)
+                                             .open(path)
+                                             .map_err(|e| CompileError::FileError(self.file_name.clone(), e))?;
+        self.compile_expr(&mut file)?;
+        self.write_out(&[0xfe], &mut file)?;
+        self.write_out(&[0xff], &mut file)?;
+        Ok(())
     }
 
-    fn compile_expr(&mut self) -> Result<(), CompileError> {
+    fn compile_expr(&mut self, file: &mut fs::File) -> Result<(), CompileError> {
         match self.current_subtree {
             AstNode::Binary(left, op, right) => {
                 self.current_subtree = left;
-                self.compile_expr();
+                self.compile_expr(file);
                 self.current_subtree = right;
-                self.compile_expr();
+                self.compile_expr(file);
                 match op.ttype {
-                    TokenType::OpPlus => self.write_out(&[0x02]),
-                    TokenType::OpMinus => self.write_out(&[0x03]),
-                    TokenType::OpStar => self.write_out(&[0x04]),
-                    TokenType::OpSlash => self.write_out(&[0x05]),
+                    TokenType::OpPlus => self.write_out(&[0x02], file),
+                    TokenType::OpMinus => self.write_out(&[0x03], file),
+                    TokenType::OpStar => self.write_out(&[0x04], file),
+                    TokenType::OpSlash => self.write_out(&[0x05], file),
                     _ => Err(CompileError::ExpectedOp(op.loc.clone()))
                 }
             },
             AstNode::Unary(op, expr) => {
                 self.current_subtree = expr;
-                self.compile_expr();
+                self.compile_expr(file);
                 match op.ttype { 
-                    TokenType::OpMinus => self.write_out(&[0x06]),
+                    TokenType::OpMinus => self.write_out(&[0x06], file),
                     _ => Err(CompileError::ExpectedOp(op.loc.clone()))
                 }
             },
             AstNode::Grouping(expr) => {
                 self.current_subtree = expr;
-                self.compile_expr()
+                self.compile_expr(file)
             },
             AstNode::Literal(val) => {
                 let index = self.const_table.len();
@@ -72,15 +71,15 @@ impl<'c> Compiler<'c> {
                     return Err(CompileError::ConstTableOverflow)
                 }
                 self.const_table.push(*val);
-                self.write_out(&[0x01])?;
-                self.write_out(&u16::to_le_bytes(index as u16))
+                self.write_out(&[0x01], file)?;
+                self.write_out(&u16::to_le_bytes(index as u16), file)
             },
             AstNode::None => panic!("неожиданное появление AstNode::None"),
         }
     }
 
-    fn write_out(&mut self, bytes: &[u8]) -> Result<(), CompileError> {
-        self.out_file.write(bytes).map(|_| ()).map_err(|e| CompileError::FileError(self.file_name.clone(), e))
+    fn write_out(&self, bytes: &[u8], file: &mut fs::File) -> Result<(), CompileError> {
+        file.write(bytes).map(|_| ()).map_err(|e| CompileError::FileError(self.file_name.clone(), e))
     }
 
     pub fn consts(&self) -> &Vec<f64> {
